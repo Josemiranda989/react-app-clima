5a0044196cd58c4b52364cf1fcea1a514e8091c4
José Miranda <josemiranda989@gmail.com>
2022-12-09 15:23:30 -0300

    clima

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { ajax } from "./tools/ajax";
+
+const App = () => {
+  return (
+    <div>
+      <label> Elije un país</label>
+      <select name="">
+        <option value="" key=""></option>
+        <option value="" key=""></option>
+        <option value="" key=""></option>
+        <option value="" key=""></option>
+        <option value="" key=""></option>
+        <option value="" key=""></option>
+      </select>
+    </div>
+  );
+};
+
+export default App;
+
+/* Rest countries
+https://restcountries.com/v3.1/all
+
+https://www.youtube.com/watch?v=A3SOd0c7a9s&list=WL&index=42
+27min
+*/
